4f025a00cd8f23484a0c64ef19c27eac35df35d0
sibivarma11 <sibivarma11@gmail.com>
2025-08-26 10:12:00 +0530

    remove unused Google Fonts import from Head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,12 +28,6 @@ export default function HomePage() {
 
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-      </Head>
-      
       <div className="min-h-screen bg-white font-[!Manrope] overflow-x-hidden">
         {/* Navigation */}
         <Navigation/>
